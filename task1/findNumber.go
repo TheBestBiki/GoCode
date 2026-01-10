a67022506fe41fa1a4941b4dea6b428b1e13b3c5
福田 <biki22@163.com>
2026-01-10 14:32:35 +0800

    任务1：找出只出现一次的数字--优化代码

--- task1/findNumber.go
+++ task1/findNumber.go
@@ -10,42 +10,42 @@ import (
  */
 func main() {
 
-	numberArr := []int{1, 1, 2, 3, 3, 3}
+	numberArr := []int{1, 2, 3, 3, 3}
 
-	number, err := findFirstNumber(numberArr)
+	uniqueNumberArr, err := findUniqueNumber(numberArr)
 	if err != nil {
 		fmt.Println("执行异常：", err)
 	} else {
-		if number == -1 {
+		// 这里就算uniqueNumberArr为nil也不会报错，可以同时处理 nil 和空切片
+		if len(uniqueNumberArr) == 0 {
 			fmt.Println("不存在只出现一次的数字")
 		} else {
-			fmt.Println("只出现一次的数字", number)
+			fmt.Println("只出现一次的数字有：", uniqueNumberArr)
 		}
 	}
 
 }
 
-func findFirstNumber(numberArr []int) (int, error) {
+func findUniqueNumber(numberArr []int) (uniqueNumberArr []int, err error) {
 	if len(numberArr) == 0 {
-		return -1, errors.New("入参数组不能为空")
+		return nil, errors.New("入参数组不能为空")
 	}
 
 	number2CountMap := make(map[int]int)
 	for _, number := range numberArr {
-		count, exists := number2CountMap[number]
-		if exists {
-			number2CountMap[number] = count + 1
-		} else {
-			number2CountMap[number] = 1
-		}
+		number2CountMap[number]++
 	}
 
 	for number, count := range number2CountMap {
 		if count == 1 {
-			return number, nil
+			uniqueNumberArr = append(uniqueNumberArr, number)
 		}
 	}
 
+	if len(uniqueNumberArr) > 0 {
+		return uniqueNumberArr, nil
+	}
+
 	fmt.Println("不存在只出现一次的数字")
-	return -1, nil
+	return nil, nil
 }
