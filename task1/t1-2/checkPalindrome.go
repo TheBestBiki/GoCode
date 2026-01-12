f133dc185e4ac4169a40ccba71dec290b7f03426
福田 <biki22@163.com>
2026-01-13 07:09:20 +0800

    任务1-2：判断是不是回文数

---
+++ task1/t1-2/checkPalindrome.go
@@ -0,0 +1,39 @@
+package main
+
+func main() {
+	palindromeNum := 11211
+	isPalindrome := validatePalindrome(palindromeNum)
+	if isPalindrome {
+		println("回文数")
+	} else {
+		println("非回文数")
+	}
+}
+
+func validatePalindrome(num int) bool {
+	// 负数不是回文数
+	if num < 0 {
+		return false
+	}
+
+	//尾数为0不是回文数
+	if num%10 == 0 {
+		return false
+	}
+
+	// 个数都是回文数
+	if num < 10 {
+		return true
+	}
+
+	originNum := num
+	reverseNum := 0
+
+	for num > 0 {
+		reverseNum = reverseNum*10 + num%10
+		num = num / 10
+	}
+
+	return originNum == reverseNum
+
+}
