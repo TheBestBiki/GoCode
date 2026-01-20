f45a57b6ca3a79c34645b67eeb3ab50361c12515
futianComp <924872898@qq.com>
2026-01-20 17:57:00 +0800

    任务2-1：指针

---
+++ task2/task2-1/changeSlice.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+/*
+实现一个函数，接收一个整数切片的指针，将切片中的每个元素乘以2。考察点 ：指针运算、切片操作。
+*/
+func main2() {
+	numArr := []int{1, 2, 3, 4}
+	changeSlice(&numArr)
+	fmt.Println(numArr)
+}
+
+func changeSlice(sliceArr *[]int) {
+	copySlice := *sliceArr
+	for i, v := range copySlice {
+		copySlice[i] = v * 2
+	}
+}
